a11a99e43d3392f6c44e717d8ac650f495d4a329
Georgina Rojas <ginarojas29@gmail.com>
2021-03-22 15:45:05 -0300

    Was made the componentization of App function.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,29 @@
+import { useMovies } from "../hooks/useMovies";
+import { MovieCard } from "./MovieCard";
+
+import "../styles/sidebar.scss";
+import React from "react";
+import { Header } from "./Header";
+
 export function Content() {
-  // Complete aqui
+  const { selectedGenre, movies } = useMovies();
+
+  return (
+    <div className="container">
+      <Header title={selectedGenre.title} />
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
